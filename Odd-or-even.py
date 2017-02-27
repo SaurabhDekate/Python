6ccd603968a1003c3af139942525c83c82b36052
Saurabh Dekate <sdekate@8x8.com>
2017-02-26 21:38:32 -0800

    2nd program

---
+++ Odd-or-even.py
@@ -0,0 +1,13 @@
+def oddeven():
+
+    number = int(input("Enter a number to find if it is even or odd : "))
+
+    if (number % 2 == 0):
+        print ("It is a even number")
+    else:
+        print ("It is a odd number")
+
+oddeven();
+
+
+
