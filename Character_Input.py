15ebfc010b8df8592e4a5d95bbc5fef44756addf
Saurabh Dekate <sdekate@8x8.com>
2017-02-26 21:13:09 -0800

    First program

---
+++ Character_Input.py
@@ -0,0 +1,10 @@
+def char_ip():
+    name = input("Enter your name")
+    age = int (input("Enter your age"))
+
+    x = 100 - age
+    cal = 2017 + x
+
+    print ("You will be 100 years old in" , cal)
+
+char_ip();
